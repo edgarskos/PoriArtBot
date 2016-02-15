dec85d3b7989e54e2c35e1e7966009ca4786e5d1
ernoma <ernoma@gmail.com>
2016-02-15 22:17:20 +0200

    Initial commit

---
+++ poriartbot.py
@@ -0,0 +1,171 @@
+
+# Overall functionality:
+# The bot reads spreadsheet data regarding public art at the city of Pori, Finland and updates Wikidata items based on the spreadsheet data. The spreadsheet has been created and is upkept by the Pori Art Museum (http://www.poriartmuseum.fi/). The 
+# 1. check if the first col contains url and if it contains check if wikidata-item exists
+# 2.1 if it does not have coords and the spreadsheet second and third col contain coords then insert coords
+# 2.2 if item exists check the existence of the rest item properties and spreadsheet cols and insert if possible
+
+import pywikibot
+import io
+import csv
+import requests
+from urllib import parse
+import time
+import re
+from pywikibot.pagegenerators import WikibaseSearchItemPageGenerator
+
+def is_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def addItemClaim(item, property_id, item_id):
+    claim = pywikibot.Claim(repo, property_id)
+    target = pywikibot.ItemPage(repo, item_id)
+    claim.setTarget(target)
+    item.addClaim(claim)
+
+
+site = pywikibot.Site("wikidata", "wikidata")
+#site = pywikibot.Site("test", "wikidata")
+repo = site.data_repository()
+
+# Real spreadsheet for the Pori public art
+sheet_url = "https://docs.google.com/spreadsheets/d/1XYeO5BNS71y2XjLfCHwDExDewKInDOCbqFW6-1gVIBU/pub?output=csv"
+#Test sheet
+#sheet_url = "https://docs.google.com/spreadsheets/d/1Qq-FF4yO-UopXifnhG8yAyOmmKTs9Bn31pEDJ0Rr-3k/pub?output=csv"
+
+coord_location_property = u'P625' # www.wikidata.org
+#coord_location_property = u'P125' # test.wikidata.org
+instance_of_property = u'P31'
+#instance_of_property = u'P82'
+publication_date_property = u'P577'
+#publication_date_property = u'P151'
+creator_property = u'P170'
+#creator_property = u'P728'
+country_property = u'P17'
+#country_property = u'P17'
+public_art_item = u'Q557141'
+#public_art_item = u'Q2225'
+sculpture_item = u'Q860861'
+#sculpture_item = u'Q2226'
+Finland_item = u'Q33'
+#Finland_item = u'Q1672'
+
+r = requests.get(sheet_url)
+r.encoding = 'utf-8'
+#pywikibot.output(r.encoding)
+#pywikibot.output((r.text)
+reader = csv.reader(io.StringIO(r.text))
+
+next(reader, None) # skip the header row
+
+for row in reader:
+    #pywikibot.output(row)
+        
+    parsed_url = parse.urlparse(row[0])
+    #pywikibot.output(parsed_url)
+    if parsed_url.scheme != '':
+        item_id = parsed_url.path.split('/')[2]
+        pywikibot.output(item_id)
+        item = pywikibot.ItemPage(repo, item_id)
+        if item.exists():
+            #pywikibot.output(item.title)
+            #pywikibot.output(item.claims)
+
+            # If sheet includes coordinates for the piece of art and if the wikidata item does not,
+            # then add coordinates for the wikidata item
+            if is_float(row[1]) and is_float(row[2]):
+                lat = float(row[2])
+                lng = float(row[1])
+
+                if not coord_location_property in item.claims:
+                    claim = pywikibot.Claim(repo, coord_location_property)
+                    target = pywikibot.Coordinate(lat, lng, precision=0.0001)
+                    claim.setTarget(target)
+                    try:
+                        item.addClaim(claim)
+                    except CoordinateGlobeUnknownException as e:
+                        pywikibot.output(u'Skipping unsupported globe: %s' % e.args)
+                
+            # Add claims instance of public art and instance of sculpture if the item does not have them
+            if not instance_of_property in item.claims:
+                pywikibot.output(u'adding instance of public art and sculpture claims')
+                addItemClaim(item, instance_of_property, public_art_item)
+                addItemClaim(item, instance_of_property, sculpture_item)
+            else:
+                # Check if instance_of_property has targets public art or sculpture
+                # and if not then add
+                found_public_art = False
+                found_sculpture = False
+                for claim in item.claims[instance_of_property]:
+                    #pywikibot.output(claim.getTarget().title())
+                    if claim.getTarget().title() == public_art_item:
+                        found_public_art = True
+                    elif claim.getTarget().title() == sculpture_item:
+                        found_sculpture = True
+
+                if not found_public_art:
+                    pywikibot.output(u'adding instance of public art claim')
+                    addItemClaim(item, instance_of_property, public_art_item)
+                if not found_sculpture:
+                    pywikibot.output(u'adding instance of sculpture claim')
+                    addItemClaim(item, instance_of_property, sculpture_item)
+
+            if not country_property in item.claims: # Add country
+                pywikibot.output(u'adding country claim')
+                addItemClaim(item, country_property, Finland_item)
+                
+            if not publication_date_property in item.claims: # Add publication date
+                claim = pywikibot.Claim(repo, publication_date_property)
+                try:
+                    pub_time = time.strptime(row[5], "%m/%d/%Y")
+                    pywikibot.output(u'adding publication date claim')
+                    pywikibot.output("" + str(pub_time.tm_mday) + "." + str(pub_time.tm_mon) + "." + str(pub_time.tm_year))
+                    target = pywikibot.WbTime(year=pub_time.tm_year, month=pub_time.tm_mon, day=pub_time.tm_mday)
+                    claim.setTarget(target)
+                    item.addClaim(claim)
+                except ValueError:
+                    try:
+                        pub_time = time.strptime(row[5], "%d.%m.%Y")
+                        pywikibot.output(u'adding publication date claim')
+                        pywikibot.output("" + str(pub_time.tm_mday) + "." + str(pub_time.tm_mon) + "." + str(pub_time.tm_year))
+                        target = pywikibot.WbTime(year=pub_time.tm_year, month=pub_time.tm_mon, day=pub_time.tm_mday)
+                        claim.setTarget(target)
+                        item.addClaim(claim)
+                    except ValueError:
+                        # If could not create date via strptime, try to find four digits and assume them to be the publication year
+                        m = re.search('\d{4}', row[5])
+                        if m != None:
+                            pywikibot.output(u'adding publication date claim')
+                            pywikibot.output(m.group(0))
+                            target = pywikibot.WbTime(year=m.group(0))
+                            claim.setTarget(target)
+                            item.addClaim(claim)
+                
+            if row[7] != '': # The piece of art has a Finnish Wikipedia page?
+                #pywikibot.output(item.sitelinks)
+                if u'fiwiki' not in item.sitelinks:
+                    pywikibot.output(u'adding Wikipedia link')
+                    parts = row[7].split('/')
+                    page_title = parts[len(parts)-1].replace("_", " ")
+                    page_title = parse.unquote(page_title)
+                    #pywikibot.output(page_title)
+                    item.setSitelink(sitelink={'site': 'fiwiki', 'title': page_title}, summary=u'Set sitelink')
+                        
+            if row[3] != '': # Add Finish label if the wikidata item does not have it
+                #pywikibot.output(item.labels)
+                if u'fi' not in item.labels:
+                    pywikibot.output(u'adding Finish label')
+                    item.editLabels(labels={'fi': row[3]}, summary=u'Add Finnish label')
+                
+            if creator_property not in item.claims and row[4] != '': # Add creator claim if the item does not have it and wikidata item of the creator is found via the page generator
+                gen = WikibaseSearchItemPageGenerator(row[4], language="fi", site=site)
+                if gen:
+                    items = list(enumerate(gen))
+                    if len(items) > 0:
+                        pywikibot.output(u'adding creator claim')
+                        item_id = items[0][1].title()
+                        addItemClaim(item, creator_property, item_id)
