9432b0bb1000117de7e697c87f43508b9605d52b
ernoma <ernoma@gmail.com>
2016-02-16 16:13:01 +0200

    Take redirect pages into account and comment out part of the claim edits

--- poriartbot.py
+++ poriartbot.py
@@ -89,6 +89,8 @@ for row in reader: # Go through spreadsheet rows
         item_id = parsed_url.path.split('/')[2]
         pywikibot.output(item_id)
         item = pywikibot.ItemPage(repo, item_id)
+        if item.isRedirectPage():
+            item = item.getRedirectTarget()
         if item.exists():
             #pywikibot.output(item.title)
             #pywikibot.output(item.claims)
@@ -109,59 +111,59 @@ for row in reader: # Go through spreadsheet rows
                         pywikibot.output(u'Skipping unsupported globe: %s' % e.args)
                 
             # Add claims instance of public art and instance of sculpture if the item does not have them
-            if not instance_of_property in item.claims:
-                pywikibot.output(u'adding instance of public art and sculpture claims')
-                addItemClaim(item, instance_of_property, public_art_item)
-                addItemClaim(item, instance_of_property, sculpture_item)
-            else:
+            #if not instance_of_property in item.claims:
+            #    pywikibot.output(u'adding instance of public art and sculpture claims')
+            #    addItemClaim(item, instance_of_property, public_art_item)
+            #    addItemClaim(item, instance_of_property, sculpture_item)
+            #else:
                 # Check if instance_of_property has targets public art or sculpture
                 # and if not then add
-                found_public_art = False
-                found_sculpture = False
-                for claim in item.claims[instance_of_property]:
-                    #pywikibot.output(claim.getTarget().title())
-                    if claim.getTarget().title() == public_art_item:
-                        found_public_art = True
-                    elif claim.getTarget().title() == sculpture_item:
-                        found_sculpture = True
+            #    found_public_art = False
+            #    found_sculpture = False
+            #    for claim in item.claims[instance_of_property]:
+            #        #pywikibot.output(claim.getTarget().title())
+            #        if claim.getTarget().title() == public_art_item:
+            #            found_public_art = True
+            #        elif claim.getTarget().title() == sculpture_item:
+            #            found_sculpture = True
 
-                if not found_public_art:
-                    pywikibot.output(u'adding instance of public art claim')
-                    addItemClaim(item, instance_of_property, public_art_item)
-                if not found_sculpture:
-                    pywikibot.output(u'adding instance of sculpture claim')
-                    addItemClaim(item, instance_of_property, sculpture_item)
+            #    if not found_public_art:
+            #        pywikibot.output(u'adding instance of public art claim')
+            #        addItemClaim(item, instance_of_property, public_art_item)
+            #    if not found_sculpture:
+            #        pywikibot.output(u'adding instance of sculpture claim')
+            #        addItemClaim(item, instance_of_property, sculpture_item)
 
             if not country_property in item.claims: # Add country?
                 pywikibot.output(u'adding country claim')
                 addItemClaim(item, country_property, Finland_item)
                 
-            if not publication_date_property in item.claims: # Add publication date?
-                claim = pywikibot.Claim(repo, publication_date_property)
-                try: # Try to parse date in format 11/23/1990
-                    pub_time = time.strptime(row[5], "%m/%d/%Y")
-                    pywikibot.output(u'adding publication date claim')
-                    pywikibot.output("" + str(pub_time.tm_mday) + "." + str(pub_time.tm_mon) + "." + str(pub_time.tm_year))
-                    target = pywikibot.WbTime(year=pub_time.tm_year, month=pub_time.tm_mon, day=pub_time.tm_mday)
-                    claim.setTarget(target)
-                    item.addClaim(claim)
-                except ValueError:
-                    try: # Try to parse date in format 23.11.1990
-                        pub_time = time.strptime(row[5], "%d.%m.%Y")
-                        pywikibot.output(u'adding publication date claim')
-                        pywikibot.output("" + str(pub_time.tm_mday) + "." + str(pub_time.tm_mon) + "." + str(pub_time.tm_year))
-                        target = pywikibot.WbTime(year=pub_time.tm_year, month=pub_time.tm_mon, day=pub_time.tm_mday)
-                        claim.setTarget(target)
-                        item.addClaim(claim)
-                    except ValueError:
+            #if not publication_date_property in item.claims: # Add publication date?
+            #    claim = pywikibot.Claim(repo, publication_date_property)
+            #    try: # Try to parse date in format 11/23/1990
+            #        pub_time = time.strptime(row[5], "%m/%d/%Y")
+            #        pywikibot.output(u'adding publication date claim')
+            #        pywikibot.output("" + str(pub_time.tm_mday) + "." + str(pub_time.tm_mon) + "." + str(pub_time.tm_year))
+            #        target = pywikibot.WbTime(year=pub_time.tm_year, month=pub_time.tm_mon, day=pub_time.tm_mday)
+            #        claim.setTarget(target)
+            #        item.addClaim(claim)
+            #    except ValueError:
+            #        try: # Try to parse date in format 23.11.1990
+            #            pub_time = time.strptime(row[5], "%d.%m.%Y")
+            #            pywikibot.output(u'adding publication date claim')
+            #            pywikibot.output("" + str(pub_time.tm_mday) + "." + str(pub_time.tm_mon) + "." + str(pub_time.tm_year))
+            #            target = pywikibot.WbTime(year=pub_time.tm_year, month=pub_time.tm_mon, day=pub_time.tm_mday)
+            #            claim.setTarget(target)
+            #            item.addClaim(claim)
+            #        except ValueError:
                         # If could not create date via strptime, try to find four digits and assume them to be the publication year
-                        m = re.search('\d{4}', row[5])
-                        if m != None:
-                            pywikibot.output(u'adding publication date claim')
-                            pywikibot.output(m.group(0))
-                            target = pywikibot.WbTime(year=m.group(0))
-                            claim.setTarget(target)
-                            item.addClaim(claim)
+            #            m = re.search('\d{4}', row[5])
+            #            if m != None:
+            #                pywikibot.output(u'adding publication date claim')
+            #                pywikibot.output(m.group(0))
+            #                target = pywikibot.WbTime(year=m.group(0))
+            #                claim.setTarget(target)
+            #                item.addClaim(claim)
                 
             if row[7] != '': # The piece of art has a Finnish Wikipedia page?
                 #pywikibot.output(item.sitelinks)
